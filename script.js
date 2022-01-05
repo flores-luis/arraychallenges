8bbef1e239179e43ad83b40c146cfed1aeb6ddc6
luis6212 <ricardoluis6212@gmail.com>
2022-01-04 18:34:59 -0800

    complete 4th challenge

--- script.js
+++ script.js
@@ -91,3 +91,31 @@ function reverse(arr) {
    
 var result = reverse(["a", "b", "c", "d", "e"]);
 console.log(result); // we expect back ["e", "d", "c", "b", "a"]
+
+
+/*
+-Fibonacci Array---------------------------------------------------
+
+Fibonacci numbers have been studied for years and appear often in nature. 
+Write a function that will return an array of Fibonacci numbers up to a 
+given length n. Fibonacci numbers are calculated by adding the last two 
+values in the sequence together. So if the 4th value is 2 and the 5th 
+value is 3 then the next value in the sequence is 5.
+
+*/
+
+function fibonacciArray(n) {
+    // the [0, 1] are the starting values of the array to calculate the rest from
+    var fibArr = [0, 1];
+    // your code here
+    if (fibArr.length < n){
+        for (a=2; a < n; a++){
+            endNum = ((fibArr[fibArr.length - 1]) + (fibArr[fibArr.length - 2]));
+            fibArr.push(endNum);   
+        }
+    }
+    return fibArr;
+}
+   
+var result = fibonacciArray(10);
+console.log(result);
